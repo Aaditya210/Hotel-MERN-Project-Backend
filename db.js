c2ce53feb5015f8f8c8503b8ab7ed8ad7bd647d4
Aaditya210 <aadityasardar912@gmail.com>
2024-07-08 18:09:42 +0530

    version1.2

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 
 // Define the MongoDB connection URL
-const mongoURL ="mongodb://127.0.0.1:27017/hotels";
+//const mongoURL =MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGODB_URL;
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL)
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
-
+require('dotenv').config();
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.json()); // req.body
 
-
+const PORT = 3000; 
 
 
 app.get('/', function (req, res) {
@@ -19,8 +19,10 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 // Use the routers
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
+
+
   
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('listening on port 3000');
 })
 //comment added for testing
